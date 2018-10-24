28927e0a9190b408e89759b16ffe139fd84f7328
ovimedia <info@ovimedia.es>
2018-10-24 15:13:40 +0200

    Update ovimedia_meta_tags.php

--- ovimedia_meta_tags.php
+++ ovimedia_meta_tags.php
@@ -5,7 +5,7 @@ Description: Plugin que genera meta tags en el sitio web.
 Author: Ovi García - ovimedia.es
 Author URI: http://www.ovimedia.es/
 Text Domain: ovimedia_meta_tags
-Version: 0.2
+Version: 0.2.1
 Plugin URI: http://www.ovimedia.es/
 */
 
@@ -16,18 +16,42 @@ if ( ! class_exists( 'ovimedia_meta_tags' ) )
         function __construct() 
         {   
             add_action( 'wp_head',  array( $this, 'wmt_add_tags') );
+            add_action( 'customize_register', array( $this, 'wmt_customize_register') );
+        }
+
+        public function wmt_customize_register( $wp_customize ) 
+        {
+            $wp_customize->add_section('meta_tags', array(
+                'priority' => 100,
+                'title' => "Meta Tags"
+            ));
+        
+            $wp_customize->add_setting('wmt_logo', array(
+                'default' => get_option("wmt_logo", ""),
+                'type' => "option"
+            ));
+        
+            $wp_customize->add_control('wmt_logo', array(
+                'label'   =>  'Logo URL',
+                'section' => 'meta_tags',
+                'type'    => 'text',
+            ));
         }
 
         function wmt_add_tags()
         {
             global $post;
 
-	    $type = "Article";
+	        $type = "Article";
 
-            if($post->post_type == "post") $type = "Post";
+            //if($post->post_type == "post") $type = "Post";
 
-            $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+            //$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
             $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
+
+            $image = get_option("wmt_logo");
+
+            list($width, $height) = getimagesize($image);
 
             $description =get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true);
             $keywords = get_post_meta(get_the_ID(), '_yoast_wpseo_focuskw', true);
@@ -60,7 +84,13 @@ if ( ! class_exists( 'ovimedia_meta_tags' ) )
             {"@context":"http:\/\/schema.org","@type":"<?php echo $type; ?>",
             "publisher":{"@type":"Organization","name":"<?php echo get_bloginfo(); ?>",
             "description":<?php echo json_encode(get_bloginfo('description')); ?>,
-            "url":<?php echo json_encode(get_home_url()); ?>,"sameAs":[]},
+            "url":<?php echo json_encode(get_home_url()); ?>,"sameAs":[],
+            "logo":[{"@type":"ImageObject",
+            "name":<?php echo json_encode($post->post_title); ?>,
+            "":<?php echo json_encode($post->post_title); ?>,
+            "url":<?php echo json_encode($image); ?>,
+            "contentUrl":<?php echo json_encode($image); ?>,
+            "encodingFormat":"image\/<?php echo substr($image, strrpos($image, '.') + 1); ?>"}]},
             "author":{"@type":"Person",
             "name":<?php echo json_encode(get_the_author()); ?>,
             "image":[{"@type":"ImageObject",
@@ -79,20 +109,16 @@ if ( ! class_exists( 'ovimedia_meta_tags' ) )
             "headline":<?php echo json_encode($post->post_title); ?>,
             "description": <?php echo json_encode($description); ?>,
             "articleSection":<?php echo json_encode(substr($postterms, 0, -1)); ?>,
-            "keywords":<?php echo json_encode(substr($postterms, 0, -1)); ?>
-            <?php if(get_the_post_thumbnail($post->ID) != ""){ ?>
-            ,
+            "keywords":<?php echo json_encode(substr($postterms, 0, -1)); ?>,
             "image":[{"@type":"ImageObject",
             "name":<?php echo json_encode($post->post_title); ?>,
-            "url":<?php echo json_encode($image[0]); ?>,
+            "url":<?php echo json_encode($image); ?>,
             "sameAs":<?php echo json_encode(get_permalink()); ?>,
             "thumbnailUrl":<?php echo json_encode($thumbnail[0]); ?>,
-            "contentUrl":<?php echo json_encode($image[0]); ?>,
-            "width": <?php echo json_encode($image[1]); ?>,"height": <?php echo json_encode($image[2]); ?>,
-            "encodingFormat":"image\/<?php echo substr($image[0], strrpos($image[0], '.') + 1); ?>"}],
-            "thumbnailUrl":<?php echo json_encode($thumbnail[0]); ?>
-
-            <?php } ?>}
+            "contentUrl":<?php echo json_encode($image); ?>,
+            "width": <?php echo json_encode($width); ?>,"height": <?php echo json_encode($height); ?>,
+            "encodingFormat":"image\/<?php echo substr($image, strrpos($image, '.') + 1); ?>"}],
+            "thumbnailUrl":<?php echo json_encode($thumbnail[0]); ?>}
             </script>
             
             <!-- END Metadata Ovimedia Meta Tags plugin -->
